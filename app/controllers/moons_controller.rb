af19b244d96e39bc52de0a3394f4207980464086
Tony_Huang <tong@Tony-Huangs-MacBook-Pro.local>
2019-02-11 19:04:51 -0500

    add authorization

--- app/controllers/moons_controller.rb
+++ app/controllers/moons_controller.rb
@@ -1,4 +1,7 @@
 class MoonsController < ApplicationController
+    before_action :authorized
+    skip_before_action :authorized, only: [:index]
+
     def index
         @moons=Moon.all
     end
--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -1,4 +1,6 @@
 class PlanetsController < ApplicationController
+    before_action :authorized
+    skip_before_action :authorized, only: [:index]
 
     def index
         @planets=Planet.all
