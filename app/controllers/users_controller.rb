4918eda5ec12da3ec33363ecb21e6fa004400bb6
Tony_Huang <tong@Tony-Huangs-MacBook-Pro.local>
2019-02-12 17:20:21 -0500

    add styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+    before_action :authorized
+    skip_before_action :authorized, only: [:new, :create]
 
    def new
         flash[:error]=[]
@@ -7,6 +9,7 @@ class UsersController < ApplicationController
    
    def create
         user=User.new(user_params)
+        byebug
         if params[:user][:password] == params[:user][:confirm_password]
             user.save
             session[:user_id]=user.id
@@ -22,6 +25,27 @@ class UsersController < ApplicationController
       @user=User.find(params[:id])
    end
 
+   def edit
+     @user=User.find(params[:id])
+   end
+
+   def update
+      @user=User.find(params[:id])
+      if @user.valid? && params[:user][:password] == params[:user][:confirm_password]
+        @user.update(user_params)
+        redirect_to "/home/users/#{@user.id}"
+    else
+        flash[:error]=[]
+        flash[:error]=user.errors.full_messages
+        render :new
+    end
+   end
+
+   def destroy
+      User.find(params[:id]).destroy
+      session.delete :user_id
+      redirect_to "/home"
+   end
 
    private
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
     has_many :moons
     has_many :planets, through: :moons
+    validates :email, uniqueness: true
     has_secure_password
+
 end
