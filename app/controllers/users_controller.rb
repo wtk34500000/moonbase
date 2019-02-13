2d0e0f6b0df532522758e790381c3b258381dd9e
Tony_Huang <tong@Tony-Huangs-MacBook-Pro.local>
2019-02-13 11:23:54 -0500

    fix flash error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,20 +3,19 @@ class UsersController < ApplicationController
     skip_before_action :authorized, only: [:new, :create]
 
    def new
-        flash[:error]=[]
         @user=User.new
    end
    
    def create
         user=User.new(user_params)
-        if params[:user][:password] == params[:user][:confirm_password]
+        if user.valid? && params[:user][:password] == params[:user][:confirm_password]
             user.save
             session[:user_id]=user.id
             redirect_to "/home/users/#{user.id}"
         else
             flash[:error]=[]
             flash[:error]=user.errors.full_messages
-            render :new
+            redirect_to "/home/users/new"
         end
    end
 
