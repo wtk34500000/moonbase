5a91fb5a00838591630175eb6d0d7747a6d37f53
rchow1944 <rchow2@buffalo.edu>
2019-02-13 09:36:34 -0500

    added container fluid

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,4 +1,11 @@
 class Planet < ApplicationRecord
     has_many :moons
     has_many :users, through: :moons
+
+    #If 1 user owns all moons of planet then they own the planet
+    def has_owner?
+      #code
+      uniq_users = self.moons.map{|moon| moon.user_id}.uniq
+      uniq_users.count == 1 && !uniq_users[0].nil?
+    end
 end
